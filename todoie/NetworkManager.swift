7e8d597c7f5c573ed3e5843681b6c7b98572575b
mustii <mustii@mmk.one>
2018-12-19 21:57:14 +0300

    Removed all unneeded files

--- todoie/NetworkManager.swift
+++ todoie/NetworkManager.swift
@@ -13,23 +13,4 @@ import Foundation
  */
 class NetworkManager {
     
-    // The base URL require for the NetworkManager to work
-    private let baseURL: URL
-    
-    /**
-     Takes the base url of the application so we can fetch data according to it
-     - Parameters:
-        - url: the Base url of the application
-     */
-    init(url: URL) {
-        baseURL = url
-    }
-    
-    /**
-     Fetch the status okay from the server from the backend
-     - returns: Network Operation that should be added to the operations queue
-     */
-    func fetchStatusOkay() -> NetworkOperation {
-        return NetworkOperation(url: baseURL.appendingPathComponent("api"))
-    }
 }
