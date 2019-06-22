2c8d6402c86bd8486becbdeecff178d5c91d570c
mustii <mustii@mmk.one>
2019-06-22 15:01:51 +0300

    Updated gitignore

--- Pods/Target Support Files/Pods-todoieTests/Pods-todoieTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_todoieTests : NSObject
-@end
-@implementation PodsDummy_Pods_todoieTests
-@end
--- Pods/Target Support Files/Pods-todoieUITests/Pods-todoieUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_todoieUITests : NSObject
-@end
-@implementation PodsDummy_Pods_todoieUITests
-@end
